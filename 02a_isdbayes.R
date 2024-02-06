15a7b557e6d07f58e5faf640bdc8355d3401b17a
justin <jfpomeranz@gmail.com>
2024-02-06 20:48:59 +0000

    02a from server

---
+++ 02a_isdbayes.R
@@ -0,0 +1,119 @@
+# isd bayes fitting
+# script 02 a
+# just the Neon, Perkins, O'Gorman and Brazil data sets
+
+# start with 4 chains, 10 iterations and save the results
+# fit again with 500 iterations and save, then 2000
+
+devtools::install_github("jswesner/isdbayes", upgrade = FALSE)
+
+library(tidyverse)
+library(brms)
+library(isdbayes)
+
+dat <- readRDS("derived_data/filtered_size_jan-11.RDS")
+
+# dat %>%
+#   select(dat_id, site) %>%
+#   unique() %>% View
+
+filtered_vector <- c(
+  "size_spectra_bio_BA.xlsx",
+  "size_spectra_bio_MG.xlsx",
+  "size_spectra_bio_PA.xlsx",
+  "size_spectra_bio_SP.xlsx",
+  "df_O_Gorman_1.xlsx",
+  "df_Perkins.xlsx" ,
+  "df_Pomeranz.xlsx"
+)
+
+test_dat <- dat %>%
+  filter(dat_id %in% filtered_vector) %>%
+  group_by(dat_id, site) %>%
+  mutate(xmin = min(body_mass),
+         xmax = max(body_mass))
+
+#test_dat %>% pull(site) %>% unique() %>% sample(5)
+
+# test_dat <- test_dat %>%
+#   filter(site %in% c("Upper Llugwy",
+#                      "P21",
+#                      "River Lyde",
+#                      "Hengill IS7 B",
+#                      "Narrator Brook")) %>%
+  # group_by(dat_id, site) %>%
+  # mutate(xmin = min(body_mass),
+  #        xmax = max(body_mass))
+
+# isdbayes ###
+
+# priors ####
+# get priors
+# get_prior(body_mass | vreal(ind_n, xmin, xmax) ~ (1|site), 
+#           data = test_dat,
+#           stanvars = stanvars,
+#           family = paretocounts())
+# set priors
+bprior <- c(prior(normal(-1.3,0.4), class = Intercept),
+            prior(exponential(2), class = sd))
+
+# plot(density(rnorm(1000, -1.3, 0.4)))
+# plot(density(rexp(1000, 2)))
+# move mouse app
+
+# fit model ####
+# test_dat <- dat
+
+#fit1 = 4 chains, 10 iter
+
+fit1_start <- Sys.time()
+iter = 10
+fit1 = brm(body_mass | vreal(ind_n, xmin, xmax) ~ (1|site), 
+           data = test_dat,
+           stanvars = stanvars,
+           prior = bprior,
+           family = paretocounts(),
+           chains = 4,
+           cores = 4,
+           iter = iter)
+
+fit1_end <- Sys.time()
+fit1_run <- fit1_end - fit1_start
+
+saveRDS(fit1_run, paste0("results/fit1_run_", Sys.Date(), ".rds"))
+saveRDS(fit1, paste0("results/fit1_4c_", iter, "i_", Sys.Date(), ".rds"))
+
+# 500 iter
+iter = 500
+fit2_start <- Sys.time()
+fit2 = brm(body_mass | vreal(ind_n, xmin, xmax) ~ (1|site), 
+           data = test_dat,
+           stanvars = stanvars,
+           prior = bprior,
+           family = paretocounts(),
+           chains = 4,
+           iter = iter)
+
+fit2_end <- Sys.time()
+fit2_run <- fit2_end - fit2_start
+saveRDS(fit2_run, paste0("results/fit2_run_", Sys.Date(), ".rds"))
+saveRDS(fit2, paste0("results/fit2_4c_", iter, "i_", Sys.Date(),".rds"))
+
+
+# 500 iter
+iter = 2000
+fit3_start <- Sys.time()
+fit3 = brm(body_mass | vreal(ind_n, xmin, xmax) ~ (1|site), 
+           data = test_dat,
+           stanvars = stanvars,
+           prior = bprior,
+           family = paretocounts(),
+           chains = 4,
+           iter = iter)
+
+fit3_end <- Sys.time()
+fit3_run <- fit3_end - fit3_start
+saveRDS(fit3_run, paste0("results/fit3_run_", Sys.Date(), ".rds"))
+saveRDS(fit3, paste0("results/fit3_4c_", iter, "i_", Sys.Date(),".rds"))
+
+
