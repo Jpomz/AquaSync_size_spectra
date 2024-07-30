3bec42edeaa8667b6425ec46ba7739db721a76c1
Jpomz <jfpomeranz@gmail.com>
2024-07-30 15:12:38 -0600

    big edit. Split data, capture error messages, export mle_df, binned_list, and biomass_df

--- 02_format_mle_bins.R
+++ 02_format_mle_bins.R
@@ -8,71 +8,90 @@ source("bin_data.R")
 
 raw_orig <- readRDS("derived_data/formatted_files_stitched_filtered_July-2024.RDS")
 
-site_info <- raw_orig %>%
-  select(group_id, organism_group, organism_groups, geographical_latitude) %>%
+raw_simp <- raw_orig |>
+  ungroup()
+range(raw_simp$group_id)
+
+fish_and_inverts <- raw_simp |>
+  filter(organism_groups == "Invertebrates + Fish") |>
+  mutate(analysis_group = "Invertebrates + Fish")
+
+raw_simp <- raw_simp |>
+  mutate(analysis_group = organism_group)
+
+dim(raw_simp) + dim(fish_and_inverts) 
+
+raw_simp <- bind_rows(raw_simp, fish_and_inverts)
+dim(raw_simp)
+
+range(raw_simp$group_id)
+
+raw_simp <- raw_simp |>
+  group_by(group_id, analysis_group) |>
+  mutate(analysis_id = cur_group_id()) |>
+  ungroup()
+
+raw_simp |> 
+  filter(dat_id == "df_NEON.xlsx") |>
+  select(group_id, analysis_id, analysis_group) %>%
+  distinct() %>%
+  View()
+
+range(raw_simp$group_id)
+range(raw_simp$analysis_id)
+
+site_info <- raw_simp %>%
+  select(group_id, 
+         analysis_id, analysis_group,
+         organism_group, organism_groups,
+         geographical_latitude) %>%
   distinct()
 
-#raw_orig <- readr::read_csv("example_for_andrew.csv") %>% # this was the test file for Andrew
-  #dplyr::mutate_if(is.character, factor) %>%
-  #dplyr::mutate(group_id = as.factor(group_id))
-
-raw_simp <- select(raw_orig,
+raw_simp <- select(raw_simp,
                    site_date,
-                   group_id,
+                   analysis_id,
                    body_mass,
                    ind_n)
 
 
-#group_id_vec <- unique(raw_simp$group_id) [c(500:3115)]   # group_id's in example
-
 dat_split <- raw_simp %>%
-  split(raw_simp$group_id)
+  split(raw_simp$analysis_id)
+length(dat_split)
 
-
+tictoc::tic()
 plan(cluster, workers = 10)
 
-vecDiff = 2 # bigger = more estimates, but huge CI's
+vecDiff = 20 # bigger value = more estimates, 
+            # but longer time and some huge CI's
 
-mle_start <- Sys.time()
+possibly2 <- function (.f) {
+  .f <- as_mapper(.f)
+  function(...) {
+    tryCatch(.f(...), error = function(e)  e$message)
+  }
+}
+
 mle_bin <- dat_split |>
-  future_map(possibly(function(.data){
-    fit_one_list(.data)
+  future_map(possibly2(function(.data){
+    fit_one_list(.data, vecDiff = vecDiff)
     # calcLike(
     #   negLL.fn = negLL.PLB.counts,
     #   x = .data$body_mass,
     #   c = .data$ind_n,
     #   p = -1.5,
     #   vecDiff = vecDiff)
-    }, 
-    otherwise = "Error")
+    })
   )
-mle_end <- Sys.time()
-(mle_run <- mle_end - mle_start)
 
 plan(cluster, workers = 1)
-
-
-
-#bind_rows(mle_bin)
-# s.out <- data.frame(
-#   site_date=unique(raw_simp_this_id$site_date),
-#   min.x = min(raw_simp_this_id$body_mass),
-#   max.x = max(raw_simp_this_id$body_mass),
-#   n = length(raw_simp_this_id$body_mass),
-#   MLE.b =result$MLEbin.res$MLE,
-#   MLE.b.l.ci = result$MLEbin.res$conf[1],
-#   MLE.b.u.ci =result$MLEbin.res$conf[2],
-#   p.val.k1 = result$GoF_K1$Pvalue,
-#   consistent.k1 = result$GoF_K1$consistent,
-#   p.val.k2 = result$GoF_K2$Pvalue,
-#   consistent.k2 = result$GoF_K2$consistent)
+tictoc::toc()
 
 
 mle_bin_rows <- list()
 for (i in 1:length(mle_bin)){
-  if(mle_bin[[i]][[1]] == "Error"){
+  if(is.character(mle_bin[[i]])){
     out <- data.frame(
-      group_id = as.numeric(names(mle_bin[i])),
+      analysis_id = as.numeric(names(mle_bin[i])),
       site_date=NA,
       min.x = NA,
       max.x = NA,
@@ -83,9 +102,11 @@ for (i in 1:length(mle_bin)){
       p.val.k1 = NA,
       consistent.k1 = NA,
       p.val.k2 = NA,
-      consistent.k2 = NA)
+      consistent.k2 = NA,
+      error = mle_bin[[i]][1])
   } else{
     out <- mle_bin[[i]]
+    out$error <- NA
   }
   mle_bin_rows[[i]] <- out
 }
@@ -93,42 +114,77 @@ for (i in 1:length(mle_bin)){
 
 mle_bin_res_df <- bind_rows(mle_bin_rows)
 
+mle_bin_res_df %>%
+  group_by(error) %>%
+  count()
 
-length(mle_bin_res_df$MLE.b) # 15866
-length(mle_bin_res_df$MLE.b[is.na(mle_bin_res_df$MLE.b)]) # 2387 NAs
+#### vecDiff = 1
+# time = 165
+# estimates = 14,197
 
-# how many estimates ?
-length(mle_bin_res_df$MLE.b) - length(mle_bin_res_df$MLE.b[is.na(mle_bin_res_df$MLE.b)])
-# 13,497 MLE_bin estimates
+# # A tibble: 5 × 2
+# # Groups:   error [5]
+# error                                          n
+# <chr>                                      <int>
+# 1 Need to make vecDiff larger - see R window   592
+# 2 missing value where TRUE/FALSE needed         13
+# not sure what this is but it's only 13 estimates
+
+# 3 sum(binCounts) >= minCounts is not TRUE       20
+# not a high enough count even after combining bins
+
+# 4 too many open devices                       1564 
+# pretty sure this is the same "vecDiff" error but a result of 
+# parallel computing - can't make a new plot when there 
+# is more cores than graphics devices 
+
+# 5 NA                                         14197 
+# these are the estimates
+
+#### vecDiff = 20
+# time = 1327 seconds (22 minutes)
+# estimates = 15,170
+
+# # A tibble: 5 × 2
+# # Groups:   error [5]
+# error                                          n
+# <chr>                                      <int>
+# 1 Need to make vecDiff larger - see R window   500
+# 2 missing value where TRUE/FALSE needed         13
+# 3 sum(binCounts) >= minCounts is not TRUE      192
+# 4 too many open devices                        511
+# 5 NA                                         15170
 
 
 dim(mle_bin_res_df)
+
 mle_bin_res_df <- mle_bin_res_df %>%
   left_join(site_info)
 dim(mle_bin_res_df)
 
 names(mle_bin_res_df)
 
-ggplot(na.omit(mle_bin_res_df),
+ggplot(mle_bin_res_df,
        aes(x = abs(geographical_latitude),
            y = MLE.b,
            ymin = MLE.b.l.ci,
            ymax = MLE.b.u.ci,
-           color = organism_groups)) +
+           color = analysis_group)) +
   geom_pointrange(alpha = 0.5) +
   theme_bw() +
   #geom_smooth(method = "lm") +
-  facet_wrap(~organism_groups)
+  facet_wrap(~analysis_group)
 
 ggplot(mle_bin_res_df,
        aes(x = MLE.b,
            fill = organism_groups)) +
-  geom_density(alpha = 0.5, bounds = c(-3, 0))
+  geom_density(alpha = 0.5, bounds = c(-5, 0))
 
 
+# binned tibbles ####
 mle_res_id <- mle_bin_res_df %>%
   filter(!is.na(MLE.b)) %>%
-  pull(group_id) %>%
+  pull(analysis_id) %>%
   unique()
 
 class(mle_res_id)
@@ -137,6 +193,7 @@ dat_split[[as.character(mle_res_id[1])]]
 
 bin_tibble_list <- list()
 
+tictoc::tic()
 for(i in 1:length(mle_res_id)){
   i_char <- as.character(mle_res_id[i])
   
@@ -150,14 +207,59 @@ for(i in 1:length(mle_res_id)){
   
   binned <- binned_with_peak$binVals[index_peak:nrow(binned_with_peak$binVals), ]
   
-  bin_tibble_list[[i]] <- binned
+  min_body <- binned$binMin[1]
+  
+  indiv <- binned_with_peak$indiv |>
+    filter(x >= min_body)
+  
+  bin_tibble_list[[i]] <- list(indiv = indiv,
+                               binned = binned)
 }
+tictoc::toc()
+# 141 sec
 
 names(bin_tibble_list) <- as.character(mle_res_id)
+### Biomass ###
+# sum up the individuals for each group, make a data.frame
+
+biomass_list <- list()
+for(i in 1:length(bin_tibble_list)){
+  indiv <- bin_tibble_list[[i]]$indiv
+  
+  sum_biomass <- sum(indiv$x)
+  out <- data.frame(
+    analysis_id = as.numeric(names(bin_tibble_list)[i]),
+    biomass = sum_biomass)
+  biomass_list[[i]] <- out
+}
+biomass_df <- bind_rows(biomass_list)
+biomass_df <- biomass_df %>%
+  left_join(site_info) 
+
+biomass_df|>
+  ggplot(aes(x = abs(geographical_latitude),
+             y = biomass, 
+             color = analysis_group)) +
+  geom_point() +
+  theme_bw() +
+  facet_wrap(~analysis_group) +
+  scale_y_log10()
+
+### lbn_plot ###
+# how can I extract the information to re-create the LBN plots? 
+# data in bin_tibble_list$binned
+## boxes ##
+# how to make the boxes? geom_rect, left-righ = binMin Max
+# top-bottom = high and low counts??
+## lines ##
+# LBN uses dPLB() or something I think? "lines()" in base R plotting?
+
 length(mle_res_id)
 
+# save outputs ####
 saveRDS(mle_bin_res_df, "derived_data/mle_bin_gof_result.RDS")
 saveRDS(bin_tibble_list, "derived_data/list_bin_tibbles.RDS")
+saveRDS(biomass_df, "derived_data/sum_biomass_df.RDS")
 
 
 
@@ -169,49 +271,108 @@ mle_bin_res_df %>%
   filter(is.na(MLE.b)) %>%
   pull(group_id)
 
-raw_simp_this_id <- dat_split[["254"]]
-# 254 - error in combine_bin()
-  # Error in combine_bins(binBreaks = binBreaks, binCounts = binCounts) :
-  # sum(combined_counts) == sum(binCounts) is not TRUE
-  # when I try to run through combine_bins() manually I don't get this error  
-
-raw_simp_this_id <- dat_split[["256"]]
-# same error
-raw_simp_this_id <- dat_split[["351"]]
-# same
-raw_simp_this_id <- dat_split[["382"]]
-# same
-raw_simp_this_id <- dat_split[["396"]]
-# same
-raw_simp_this_id <- dat_split[["399"]]
-# same
-raw_simp_this_id <- dat_split[["418"]]
-# same
-raw_simp_this_id <- dat_split[["420"]]
-# same
-raw_simp_this_id <- dat_split[["1531"]]
-# same
-raw_simp_this_id <- dat_split[["2394"]]
-# same
-
-raw_simp_this_id <- dat_split[["6314"]]
-# increase size of vecDiff 
 
 
-raw_simp_this_id <- dat_split[["13591"]]
-# combine_bins error
-raw_simp_this_id <- dat_split[["13569"]]
-# combine_bins error
+# possibly with error return ----------------------------------------------------
 
-raw_simp_this_id <- dat_split[["13649"]]
-# `ind_n` column is all NAs
-raw_simp_this_id <- dat_split[["13656"]]
-# `ind_n` column is all NAs
-raw_simp_this_id <- dat_split[["13656"]]
-# `ind_n` column is all NAs
+possibly2 <- function (.f) {
+  .f <- as_mapper(.f)
+  function(...) {
+    tryCatch(.f(...), error = function(e)  e$message)
+  }
+}
+
+# make a vector of just NA groups
+na_groups <- mle_bin_res_df %>%
+  filter(is.na(MLE.b)) %>%
+  pull(group_id)
+
+dat_na <- raw_simp %>%
+  filter(group_id %in% na_groups) 
+dim(raw_simp)
+dim(dat_na)
+dat_na <- dat_na %>%
+  split(dat_na$group_id)
+length(dat_na)
+
+tictoc::tic()
+plan(cluster, workers = 10)
+
+vecDiff = 2 # bigger = more estimates, but huge CI's
+# 2 = 57 seconds
+
+na_bin <- dat_na |>
+  future_map(possibly2(function(.data){
+    fit_one_list(.data, vecDiff = vecDiff)  }
+  )
+  )
+
+plan(cluster, workers = 1)
+tictoc::toc()
+
+#na_bin[[1]] %>% class()
+
+
+na_bin_res_df <- bind_rows(lapply(na_bin, as.data.frame), .id = "id")
+# View(na_bin_res_df)
+# nrow(na_bin_res_df)
+# 
+# na_bin_res_df %>%
+#   filter(is.na(MLE.b)) %>%
+#   nrow()
+# 
+# names(na_bin_res_df)
+# head(na_bin_res_df)
+
+na_bin_res_df <- na_bin_res_df %>%
+  rename(error = "X[[i]]")
+
+na_bin_res_df %>%
+  group_by(error) %>%
+  count()
+
+na_bin_res_df %>%
+  filter(str_detect(error, "sum"))
+
+na_bin_res_df %>%
+  filter(str_detect(error, "sum"))
+
+
+# looking at errors one by one --------------------------------------------
 
 
 
+# raw_simp_this_id <- dat_split[["12333"]]
+
+# group_id before filtering out small rows
+dat_split[["65"]]
+# only one row of data
+dat_split[["84"]]
+# only three rows of data
+dat_split[["86"]]
+# 40 rows of data, error, need to make vecDiff larger
+dat_split[["90"]]
+# 16 rows of data, error, need to make vecDiff larger
+
+dat_split[["9697"]]
+# make vecDiff larger
+
+
+dat_split[["10018"]]
+# error in combine_bins
+# only ~ 3 bins after peak
+dat_split[["12335"]]
+# error in combine_bins
+# only ~ 4 bins after peak
+
+dat_split[["12333"]]
+# GoF_res_K2 NaNs produced
+
+
+# running functions one by one --------------------------------------------
+
+
+suppress.warnings = TRUE
 counts <- dplyr::select(raw_simp_this_id,
                         x = body_mass,
                         counts = ind_n)
@@ -254,6 +415,71 @@ GoF_res_K2 <- GoF_PLB(bin_breaks = binBreaks,
                       b = MLEbin.res$MLE,
                       K = 2)
 
+
+
+# running combined bins line by line --------------------------------------
+
+combine_bins <- function(binBreaks,
+                         binCounts,
+                         minCounts = 5){
+  stopifnot(length(binBreaks) == length(binCounts) + 1)
+  stopifnot(sum(binCounts) >= minCounts)
+  
+  # Check each in turn, combine with subsequent one if binCounts[i] is < minCounts:
+  combined_breaks <- vector()
+  combined_counts <- vector()
+  
+  combined_counts_i <- 1      # counter for combined_counts vector
+  combined_breaks[1] <- binBreaks[1]
+  
+  i <- 1                      # counter for binCounts
+  while(i <= length(binCounts)){
+    if(binCounts[i] >= minCounts){
+      combined_counts[combined_counts_i] <- binCounts[i]
+      combined_breaks[combined_counts_i + 1] <- binBreaks[i + 1]
+      combined_counts_i <- combined_counts_i + 1
+      i <- i + 1
+    } else {
+      # Combine the next bins to get >=5 total count
+      cumul <- cumsum(binCounts[i:length(binCounts)])
+      if(max(cumul) >= minCounts){
+        first_to_five <- min(which(cumul >= minCounts))  # first index of remaining counts
+        #  to have cumulative count of
+        #  >= minCounts (default 5,
+        #  hence first_to_five),
+        #  will be index >=2
+        
+      } else {
+        first_to_five <- length(cumul)           # just take them all and fix afterwards
+      }
+      
+      combined_counts[combined_counts_i] <- cumul[first_to_five]
+      combined_breaks[combined_counts_i + 1] <- binBreaks[i + first_to_five]  # TODO CHECK THAT, may
+      # be off by 1
+      combined_counts_i <- combined_counts_i + 1
+      i <- i + first_to_five
+    }
+  }
+  
+  # But count in final bin may be <5, if so then combine with penultimate bin
+  # (which should be >=5 by definition)
+  M = length(combined_counts)
+  if(combined_counts[M] < minCounts){
+    combined_counts[M-1] <- combined_counts[M-1] + combined_counts[M]
+    combined_counts <- combined_counts[-M]
+    combined_breaks <- combined_breaks[-M]
+  }
+  
+  stopifnot(sum(combined_counts) == sum(binCounts))
+  
+  return(list(combined_breaks = combined_breaks,
+              combined_counts = combined_counts))
+}
+
+
+
+
+fit_one_list(dat_split[["1"]], vecDiff = 10, suppress.warnings = TRUE)
 
 # old code from Perkins: --------------------------------------------------
 
